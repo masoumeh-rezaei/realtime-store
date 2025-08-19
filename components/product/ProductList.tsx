f8eb36e3026f79f900a97101d29138c5b0baabeb
Masomeh Rezaei <masomehrezaei83.r@gmail.com>
2025-08-19 14:21:35 +0330

    Update productList version 2.16

--- components/product/ProductList.tsx
+++ components/product/ProductList.tsx
@@ -2,9 +2,22 @@
 
 import ProductCard from "./ProductCard";
 import { useProductList } from "@/hooks/useProductList";
+import Link from "next/link";
 
 const ProductList = () => {
     const { products } = useProductList();
+
+    if (products.length === 0) {
+        return (
+            <div className="text-center text-gray-600 mt-10">
+                No products available.{" "}
+                <Link href="/admin" className="text-blue-500 underline">
+                    Go to Admin Panel
+                </Link>{" "}
+                to add some.
+            </div>
+        );
+    }
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
